8a5df9fef1734dad0d63a2af2e9b3c5df8054d1a
alibedelov <alibedelov@gmail.com>
2026-05-05 19:14:10 +0400

    FerryThread and VehicleThread has been implemented
    Five big booms
    boom
    boom
    boom
    boom
    booooom

--- src/threads/FerryThread.java
+++ src/threads/FerryThread.java
@@ -1,4 +1,70 @@
 package threads;
 
-public class FerryThread {
+import core.WaitingQueue;
+import model.Side;
+import sync.FerryControl;
+import sync.SyncManager;
+import utils.Logger;
+import utils.Statistics;
+
+public class FerryThread extends Thread
+{
+    private SyncManager syncManager;
+    private Side currentSide;
+    private boolean simulationRunning = true;
+    private int tripCount = 0;
+
+    public FerryThread(SyncManager syncManager, Side startSide)
+    {
+        this.syncManager = syncManager;
+        this.currentSide = startSide;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            FerryControl ferryControl = syncManager.getFerryControl();
+
+            while(simulationRunning)
+            {
+                WaitingQueue queue = syncManager.getQueue(currentSide);
+
+                Logger.ferryLoadingStarted(currentSide.name());
+                ferryControl.waitForDeparture(queue);
+
+                tripCount++;
+                int currentLoad = 0; //ferryControl.getCurrentLoad(); is being awaited to be implemented
+
+                Logger.ferryDeparted(currentSide.name(), currentLoad, tripCount);
+                Statistics.recordTrip(currentLoad);
+
+                Thread.sleep(1000);
+
+                currentSide = (currentSide == Side.A) ? Side.B : Side.A;
+                Logger.ferryArrived(currentSide.name(), tripCount);
+
+                Logger.ferryUnloadingStarted(currentSide.name());
+                ferryControl.startUnloading();
+
+                Thread.sleep(500);
+
+                ferryControl.finishUnloading();
+                Logger.ferryUnloadingFinished(currentSide.name());
+
+                ferryControl.resetAfterTrip();
+            }
+        }
+        catch(InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+            System.err.println("Ferry thread was interrupted.");
+        }
+    }
+
+    public void stopSimulation()
+    {
+        this.simulationRunning = false;
+    }
 }
--- src/threads/VehicleThread.java
+++ src/threads/VehicleThread.java
@@ -1,4 +1,85 @@
 package threads;
 
-public class VehicleThread {
+import core.TollBooth;
+import core.WaitingQueue;
+import model.Side;
+import model.Vehicle;
+import sync.FerryControl;
+import sync.SyncManager;
+import utils.Logger;
+import utils.Statistics;
+
+public class VehicleThread extends Thread
+{
+    private Vehicle vehicle;
+    private SyncManager syncManager;
+    private Side originalSide;
+    private String vName;
+
+    public VehicleThread(Vehicle vehicle, SyncManager syncManager)
+    {
+        this.vehicle = vehicle;
+        this.syncManager = syncManager;
+        this.vName = vehicle.getType() + "-" + vehicle.getId();
+
+        this.originalSide = (Math.random() > 0.5) ? Side.A : Side.B;
+        this.vehicle.setCurrentSide(this.originalSide);
+
+        Logger.vehicleCreated(vName, originalSide.name());
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            boolean roundTripComplete = false;
+            int tripsComplete = 0;
+
+            while(!roundTripComplete)
+            {
+                Side currentSide = vehicle.getCurrentSide();
+                TollBooth[] tolls = syncManager.getTolls(currentSide);
+                TollBooth chosenToll = tolls[0];
+
+                Logger.vehicleEnteredToll(vName, "Toll-1", currentSide.name());
+                chosenToll.enter(this.vehicle);
+                Thread.sleep(100);
+                chosenToll.exit(this.vehicle);
+                Logger.vehicleExitedToll(vName, "Toll-1", currentSide.name());
+
+                WaitingQueue queue = syncManager.getQueue(currentSide);
+                Logger.vehicleJoinedQueue(vName, currentSide.name());
+                long queueEntryTime = Statistics.recordQueueEntry();
+                queue.enqueue(this.vehicle);
+
+                FerryControl ferryControl = syncManager.getFerryControl();
+                ferryControl.requestBoarding(this.vehicle, queue);
+
+                Statistics.recordBoarding(queueEntryTime);
+
+                Side otherSide = (currentSide == Side.A) ? Side.B : Side.A;
+                vehicle.setCurrentSide(otherSide);
+                tripsComplete++;
+                Logger.vehicleUnloaded(vName, otherSide.name());
+
+                if(tripsComplete == 2 && vehicle.getCurrentSide() == originalSide)
+                {
+                    roundTripComplete = true;
+
+                }
+                else
+                {
+                    Logger.vehicleWaiting(vName, otherSide.name());
+                    Thread.sleep(500);
+                    Logger.vehicleReturning(vName, otherSide.name());
+                }
+            }
+        }
+        catch(InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+            System.err.println("Vehicle " + vehicle.getId() + " was interrupted.");
+        }
+    }
 }
